b43e5b1932e22125f74ef8dd8ac32ebaa8101baf
boykoyd228 <94605499+boykoyd228@users.noreply.github.com>
2021-11-24 22:08:32 +0300

    Add files via upload

---
+++ в классе/18.10.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("m = ");
+        int m = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i <= m; i++)
+        {
+            int g = 0;
+            while (g < i)
+            {
+                Console.Write("*");
+                g++;
+            }
+            Console.WriteLine();
+        }
+        Console.ReadLine();
+    }
